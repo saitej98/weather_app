277416b2aab3e4e25bf014aaa52d1f172da2addd
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-07-19 23:38:52 +0530

    React app Created

---
+++ weather/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Working on Weather UI.....
+    </div>
+  );
+}
+
+export default App;
