cd8d462ae817aacc69c0810c4a9768ad543b6b63
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-07-20 03:18:56 +0530

    Added weekly weather forecast

--- weather/src/WeatherComponents/weather.jsx
+++ weather/src/WeatherComponents/weather.jsx
@@ -1,41 +1,71 @@
-import { useState } from "react";
-import axios from "axios"
+import { useState,useRef } from "react";
+import axios from "axios";
 
 function Weather() {
-    const [city, setCity] = useState("");
-    const [days, setDays] = useState([]);
+  const [city, setCity] = useState("");
+  const [days, setDays] = useState([]);
+  const hourTempArray = useRef([]);
+  const [tempgraph, setTempgraph] = useState("");
+  const [tempicon, setTempicon] = useState("");
+  const [sunrise, setSunrise] = useState("");
+  const [sunset, setSunset] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [humidity, setHumidity] = useState("");
 
+  const searchCity = () => {
+    try {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b325ed4f82c44e2e1abd0702faff7d72&units=metric`
+        )
+          .then((res) => {
+            console.log(res)
+          sevenDays(res.data.coord.lat, res.data.coord.lon);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {}
+  };
+  const sevenDays = (lat, lon) => {
+    try {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=b325ed4f82c44e2e1abd0702faff7d72&units=metric`
+        )
+        .then((res) => {
+          setDays(res.data.daily);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch {}
+  };
+  const weektemp = ( sunRise, sunSet, presure, humdity, e) => {
+    let temp = [];
+    let hrRise = new Date(sunRise * 1000).getHours();
+    let minRise = "0" + new Date(sunRise * 1000).getMinutes();
+    let hrSet = new Date(sunSet * 1000).getHours();
+    let minSet = "0" + new Date(sunSet * 1000).getMinutes();
+    let rise = hrRise + ":" + minRise();
+    let set = (hrSet % 12) + ":" + minSet();
+    let result = Array.isArray(e);
+    if (result == false) {
+      for (let x in e.temp) {
+        temp.push(e.temp[x] + "℃");
+      }
+      temp = temp.splice(0, 4);
+      hourTempArray.current = temp;
+    } else {
+      hourTempArray.current = e;
+    }
 
-    const searchCity = () => {
-        try {
-          axios
-            .get(
-              `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b325ed4f82c44e2e1abd0702faff7d72&units=metric`
-            )
-              .then((res) => {
-                  sevenDays(res.data.coord.lat, res.data.coord.lon);
+    setSunrise(rise);
+    setSunset(set);
+    setPressure(presure);
+    setHumidity(humdity);
+  };
 
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        } catch (error) {}
-    };
-    const sevenDays = (lat, lon) => {
-        try {
-          axios
-            .get(
-              `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=b325ed4f82c44e2e1abd0702faff7d72&units=metric`
-            )
-            .then((res) => {
-              setDays(res.data.daily);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        } catch {}
-      };
-    
   return (
     <div>
       <div className="bar">
@@ -45,19 +75,55 @@ function Weather() {
           className="city"
           placeholder="Enter City Name Here.."
           value={city}
-          onChange={(e) => {setCity(e.target.value)}}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
         />
         <button className="search" onClick={searchCity}>
           Search
         </button>
-          </div>
-           <div className="mapdiv">
+      </div>
+      <div className="mapdiv">
         <iframe
           title="gmap"
           name="gMap"
           className="map"
           src={`https://maps.google.com/maps?q=${city}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
         ></iframe>
+      </div>
+      <div className="daytemp">
+        {days.map((e) => (
+          <div
+            id="detaildaytemp"
+            key={e.id}
+            onClick={() => {
+              weektemp(
+                e.temp.day,
+                e.weather[0].icon,
+                e.sunrise,
+                e.sunset,
+                e.pressure,
+                e.humidity,
+                e
+              );
+            }}
+            tabIndex="1"
+          >
+            <div>
+              {new Date(`${e.dt}` * 1000).toLocaleDateString("en", {
+                weekday: "short",
+              })}
+            </div>
+            <span>{Math.ceil(e.temp.day)}℃</span>
+            <img
+              className="detailIcon"
+              src={`https://openweathermap.org/img/wn/${e.weather[0].icon}.png`}
+              alt=""
+            />
+
+            <div>{e.weather[0].main}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
