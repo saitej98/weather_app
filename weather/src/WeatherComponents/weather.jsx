7c9ff012e400732c42b4f77294ec30ab0e1a16ea
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-07-22 16:16:46 +0530

    graphs implemented

--- weather/src/WeatherComponents/weather.jsx
+++ weather/src/WeatherComponents/weather.jsx
@@ -10,10 +10,9 @@ function Weather() {
   const [humidity, setHumidity] = useState("41");
   const [sunset, setSunset] = useState("7:06");
   const [sunrise, setSunrise] = useState("6:10");
-  const [tempday, setTempday] = useState("");
+  const [tempday, setTempday] = useState("0");
   const [tempicon, setTempicon] = useState("");
   const [region, setRegion] = useState("");
-
 
   // fetch from weather api by city name...
   const searchCity = () => {
@@ -48,20 +47,20 @@ function Weather() {
   };
 
   const weektemp = (day, icon, sunRise, sunSet, pressure, humdity, e) => {
-    let temp = [];
+    let arr = [];
     let hrRise = new Date(sunRise * 1000).getHours();
     let minRise = "0" + new Date(sunRise * 1000).getMinutes();
     let hrSet = new Date(sunSet * 1000).getHours();
     let minSet = "0" + new Date(sunSet * 1000).getMinutes();
-    let rise = hrRise + ":" + minRise();
-    let set = (hrSet % 12) + ":" + minSet();
+    let rise = hrRise + ":" + minRise.substr(-2);
+    let set = (hrSet % 12) + ":" + minSet.substr(-2);
     let result = Array.isArray(e);
-    if (result === false) {
+    if (result == false) {
       for (let x in e.temp) {
-        temp.push(e.temp[x] + "℃");
+        arr.push(e.temp[x] + "℃");
       }
-      temp = temp.splice(0, 4);
-      hourTempArray.current = temp;
+      arr = arr.splice(0, 4);
+      hourTempArray.current = arr;
     } else {
       hourTempArray.current = e;
     }
@@ -124,7 +123,12 @@ function Weather() {
   return (
     <div>
       <div className="bar">
-        <button className="live" onClick={Livelocation}><img src="https://image.shutterstock.com/image-vector/vector-black-colored-map-pin-260nw-1987787240.jpg" alt="" /></button>
+        <button className="live" onClick={Livelocation}>
+          <img
+            src="https://image.shutterstock.com/image-vector/vector-black-colored-map-pin-260nw-1987787240.jpg"
+            alt=""
+          />
+        </button>
         <input
           type="text"
           className="city"
@@ -219,9 +223,7 @@ function Weather() {
           ]}
           options={{
             dataLabels: {
-              formatter: (val) => {
-             
-              },
+              formatter: (val) => {},
             },
             yaxis: {
               labels: {
@@ -233,29 +235,9 @@ function Weather() {
             xaxis: {
               categories: [
                 "12:00am",
-                "1:00am",
-                "2:00am",
-                "3:00am",
-                "4:00am",
-                "5:00am",
                 "6:00am",
-                "7:00am",
-                "8:00am",
-                "9:00am",
-                "10:00am",
-                "11:00am",
                 "12:00pm",
-                "1:00pm",
-                "2:00pm",
-                "3:00pm",
-                "4:00pm",
-                "5:00pm",
                 "6:00pm",
-                "7:00pm",
-                "8:00pm",
-                "9:00pm",
-                "10:00pm",
-                "11:00pm",
               ],
             },
           }}
